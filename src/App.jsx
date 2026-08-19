29ad00dc3046c6923a70262daf33a8e4ede8287a
ludmilabenitez <ludmilahabba@gmail.com>
2026-08-19 01:11:29 -0300

    add: checkboxes, exclude spaces and character limit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,25 @@ import { Header } from "./components/Header.jsx"
 
 function App() {
   const [text, setText] = useState("Design is the silent ambassador of your brand. Simplicity is key to effective communication, creating clarity in every interaction. A great design transforms complex ideas into elegant solutions, making them easy to understand. It blends aesthetics and functionality seamlessly.")
-
+  const [excludeSpaces, setExcludeSpaces] = useState(false)
+  const characters = excludeSpaces ? text.replace(/\s/g, "").length : text.length
+  const [limitCharacter, setLimitCharacter] = useState(false)
+  const [limitValue, setLimitValue] = useState(300)
+  const handleChangeTextArea = (e) => {
+    const value = e.target.value
+    if(limitCharacter) {
+      if (value.length <= limitValue) {
+        setText(value)
+      }
+    } else {
+      setText(value)
+    }
+  }
+  const handleChangeInputLimit = () => {
+    setLimitCharacter(!limitCharacter)
+    const newText = text.slice(0, limitValue)
+    setText(newText)
+  }
 
 
 
@@ -17,11 +35,42 @@ function App() {
 
     <textarea 
     placeholder="Write your text..."
-    onChange={(e) => setText(e.target.value)}
+    onChange={handleChangeTextArea}
     value={text}
     ></textarea>
 
-    <p>Character count.</p>
+    <div>
+      <label>
+      <input type="checkbox"
+      checked= {excludeSpaces}
+      onChange={() => setExcludeSpaces(!excludeSpaces)}
+      />
+      Exclude Spaces
+      </label>
+      <label>
+      <input type="checkbox"
+      checked= {limitCharacter}
+      onChange={handleChangeInputLimit}
+      />
+      Set Character Limit
+      </label>
+      {
+        limitCharacter && 
+        <input type="number"
+        value={limitValue}
+        onChange = {(e) => setLimitValue(e.target.value)}
+         />
+      }
+
+
+
+
+    </div>
+
+    <p>Total Characters: {characters} </p>
+
+
+
 
     
   </main>
