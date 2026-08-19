0f559ff73267f7f1f1f63e92fad03f9078bea971
ludmilabenitez <ludmilahabba@gmail.com>
2026-08-18 22:29:45 -0300

    add: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { Header } from "./components/Header.jsx"
+
 
 function App() {
-  return <h1>Hello</h1>
+  return 
+  <main>
+    <header/>
+  </main>
 }
 
 export {App}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return 
+    <header>
+      <div>
+        <p>C</p>
+        <h1>Character Counter UTN</h1>
+      </div>
+      <button>☀︎</button>
+    </header>
+}
+
+export { Header }
